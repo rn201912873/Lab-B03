229b684eb47ee1df4828670ac82ee667e5738c22
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-02-21 15:41:47 +0300

    Update app.js

--- Lab Material/Lab 5 JS Fundamentals/JS Basics/app.js
+++ Lab Material/Lab 5 JS Fundamentals/JS Basics/app.js
@@ -81,20 +81,121 @@
 // // arr.sort((a, b) => b - a) --concl.
 // console.log("Sorted", arr);
 
-// Functions
-function add(a, b) {
-    return a + b
-}
+// declaration
+// let a = 10
+// let b = 40
+
+// function add(a, b) {
+//     return a + b
+// }
+
+// const add2 = function (a, b) {
+//     return a + b
+// }
+
+// // calling and using the function
+// const result = add(a, b)
+// const result2 = add2(a, b)
+
+// console.log("add(a, b) = ", result);
+// console.log("add2(a, b) = ", result2);
+
+// arrow function
+
+// function add(a, b) {
+//     return a + b
+// }
+
+// (a, b) => a + b
+
+// const checkFailPass = grade => grade >= 50 ? "Pass" : "Fail"
+
+// function failPass(grade) {
+//     if (grade >= 50) return "Pass" 
+//      return "Fail"
+// }
+
+// // function print(a) {
+// //     console.log(a)
+// // }
+
+// a => console.log(a)
+
+//     // function scream() {
+//     //     console.log("I am screaming");
+//     // }
+
+//     () => console.log("I am screaming")
 
 
-const result = add(3, 5)
-console.log("add(3, 5) = ", result);
+// scream2()
+// print("Something to print")
+// aAdd(2, 4)
 
 
 
+// const add = (a, b) => a + b;
+// const sub = (a, b) => a - b;
+// const mul = (a, b) => a * b;
+// const div = (a, b) => a / b;
+// const square = (a) => a ** 2;
+
+// const a = 20
+// const b = 10
+
+
+// function calculator(a, b, operation) {
+//     const result = operation(a, b)
+//     return result
+// }
+
+// console.log("Calc ", calculator(a, b, add));
+// console.log("Calc ", calculator(20, 10, mul));
+
+// console.log("Calc ", calculator(50, 10, div));
+// console.log("Calc ", calculator(50, 10, (t, r) => t / r));
+// console.log("Calc ", calculator(50, 10, square));
 
 
 
+// const arr = [11, 21, 13, -4, 5, -6, -7]
+// arr.sort((a, b) => a - b)
+// console.log(arr);
 
+// // change or map
+// const cubeArray = arr.map(a => a ** 3)
+// console.log(cubeArray);
 
+// // arr.forEach(a => console.log(a))
+// const v = arr.find(a => a == -4)
+// console.log(v);
 
+const students = [
+    {
+        name: "Ali",
+        age: 25,
+        gender: "Male"
+    },
+    {
+        name: "Zahra",
+        age: 24,
+        gender: "Female"
+    },
+    {
+        name: "Mariam",
+        age: 22,
+        gender: "Female"
+    },
+    {
+        name: "Ahmed",
+        age: 23,
+        gender: "Male"
+    },
+    {
+        name: "Rashid",
+        age: 21,
+        gender: "Male"
+    },
+]
+
+// Array Methods and Arrow Functions
